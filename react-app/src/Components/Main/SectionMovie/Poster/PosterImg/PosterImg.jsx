8ce9bb20f535e9d433fb442b428bf2f068490366
yunkooo <yunkoooooo@gmail.com>
2022-11-28 23:50:55 +0900

    Fix: 이미지 경로 수정

--- react-app/src/Components/Main/SectionMovie/Poster/PosterImg/PosterImg.jsx
+++ react-app/src/Components/Main/SectionMovie/Poster/PosterImg/PosterImg.jsx
@@ -1,6 +1,8 @@
 import { PosterImgStyle } from "./PosterImgStyle";
 
 export default function PosterImg({ img }) {
-  const imgSrc = `img/PosterImg/${img}`;
+  console.log(img)
+  // const imgSrc = 'public/img/PosterImg/${img}`;
+  const imgSrc = `${process.env.PUBLIC_URL}/img/PosterImg/${img}`;
   return <PosterImgStyle tabIndex={0} src={imgSrc} />;
 }
